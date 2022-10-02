a5d91471ddc2d25ec53d8a4923cecf5b7ae3234c
1g2g <wonji126326@gmail.com>
2022-10-03 01:11:14 +0900

    #3 setting : json server

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,9 +4,10 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:8080",
+      target: "http://localhost:4000",
       changeOrigin: true,
     })
   );
 };
 //백엔드 서버로 api 요청을 위해 proxy 설정
+//npx json-server ./data.json --port 4000
