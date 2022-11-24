07d707aa9f3811e5d8cc8fa3298ca7203a16c79b
1g2g <wonji126326@gmail.com>
2022-11-24 23:08:08 +0900

    refactor : humanaction, detection

---
+++ src/lib/utils/detection.js
@@ -0,0 +1,57 @@
+import warn from "assets/cctv1.png";
+const { naver } = window;
+import { setDateFormat, setTimeFormat } from "lib/utils/humanaction";
+
+export function setTable(obj, i) {
+  console.log({ obj });
+  return {
+    order: i,
+    id: obj.id,
+    createdAt: obj.time,
+    start_time: setDateFormat(obj.time) + " " + setTimeFormat(obj.time),
+    url: obj.url,
+    cctv_id: obj.cctv_id,
+    lat: obj.cctv.y,
+    lng: obj.cctv.x,
+    address: obj.cctv.address,
+    markerX: obj.cctv.x,
+    markerY: obj.cctv.y,
+  };
+}
+export function setPath(obj) {
+  return new naver.maps.LatLng(obj.cctv.y, obj.cctv.x);
+}
+export function setMarker(map, obj, id, url, i) {
+  return new naver.maps.Marker({
+    map,
+    title: obj.cctv.address,
+    position: setPath(obj),
+    icon: {
+      content: [
+        '<div id="cctv_marker_icon" >',
+        "<div>",
+        i,
+        "</div>",
+        "<img src='" + warn + "'/>",
+        "</div>",
+      ].join(""),
+      size: new naver.maps.Size(50, 50),
+      anchor: new naver.maps.Point(12, 37),
+    },
+    draggable: true,
+    idValue: id,
+    uriValue: url,
+  });
+}
+export function setPolyline(map, prev, cur) {
+  return new naver.maps.Polyline({
+    map: map,
+    path: [prev, cur],
+    strokeLineCap: "round",
+    strokeWidth: "0.025",
+    strokeLineJoin: "round",
+    strokeWeight: 8,
+    strokeColor: "#2840a5",
+    strokeOpacity: 0.8,
+  });
+}
---
+++ src/lib/utils/humanaction.js
@@ -0,0 +1,88 @@
+import moment from "moment";
+import "moment/locale/ko";
+const { naver } = window;
+export function setDateFormat(time) {
+  return moment(time).format("YY.MM.DD");
+}
+export function setTimeFormat(time) {
+  return moment(time).format("hh:mm:ss");
+}
+export function getElem(obj) {
+  const elem = {
+    id: obj.id,
+    type: obj.type,
+    createdAt: obj.createdAt,
+    date: moment(obj.start_time).format("YY.MM.DD"),
+    start_time: setTimeFormat(obj.start_time),
+    end_time: setTimeFormat(obj.end_time),
+    url: obj.url,
+    cctv_id: obj.cctv.id,
+    lat: obj.cctv.position.y,
+    lng: obj.cctv.position.x,
+    address: obj.cctv.address,
+  };
+  return elem;
+}
+export function setMarker(
+  map,
+  obj,
+  detectData,
+  setDetectData,
+  setModalOpen,
+  setBlur,
+  type,
+  color
+) {
+  var markerPosition;
+  if (type === "_dot") {
+    markerPosition = new naver.maps.LatLng(
+      parseFloat(obj.position.y),
+      parseFloat(obj.position.x)
+    );
+  } else
+    markerPosition = new naver.maps.LatLng(
+      parseFloat(obj.cctv.position.y),
+      parseFloat(obj.cctv.position.x)
+    );
+  const marker = new naver.maps.Marker({
+    map,
+    title: "Green",
+    position: markerPosition,
+    icon: {
+      content: [
+        '<div className="cs_mapbridge" id="cctv_marker',
+        type,
+        color,
+        '" >',
+        "<div>",
+        obj.id,
+        "</div>",
+        "</div>",
+      ].join(""),
+      size: new naver.maps.Size(10, 10),
+      anchor: new naver.maps.Point(19, 58),
+    },
+    draggable: true,
+    idValue: obj.id,
+    urlValue: obj.url,
+    startValue: obj.start_time,
+    endValue: obj.end_time,
+  });
+  marker.addListener("click", function (e) {
+    //obj.id로 api 요청
+    console.log(e.overlay.urlValue);
+    setDetectData({
+      ...detectData,
+      id: e.overlay.idValue,
+      url: e.overlay.urlValue,
+      start: e.overlay.startValue,
+      end: e.overlay.endValue,
+    });
+    setModalOpen(true);
+    setBlur(true);
+  });
+}
+export function pushToList(socketData, socketList, obj, elem) {
+  socketList.push(elem);
+  socketData.push(elem);
+}
